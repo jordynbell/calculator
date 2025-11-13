f6fafa4a71a74b7937c85bad82e378357c1c21b2
Jordyn Bell <Jordyn@Envisionafrica.co.za>
2025-11-13 14:42:50 +0200

    Added base react project. Added asp.net core web api project + C# Library project + MSTest project for test cases. Attempting a TDD approach to my test cases.

---
+++ src/calculator/Calculator.Core/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Core
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Divide(double a, double b) => b == 0 ? throw new DivideByZeroException("Cannot divide by zero.") : a / b;
+        public double Multiply(double a, double b) => a * b;
+        public double PowerOf(double a, double b) => Math.Pow(a, b);
+    }
+}
---
+++ tests/Calculator.Core.Tests/CalculatorTest.cs
@@ -0,0 +1,83 @@
+﻿namespace Calculator.Test
+{
+    [TestClass]
+    public sealed class CalculatorTest
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Act
+            var actual = calculator.Add(2, 3);
+            // Assert
+            Assert.AreEqual(5, actual);
+        }
+
+        [TestMethod]
+        public void Subtract_TwoNumbers_ReturnsDifference()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Act
+            var actual = calculator.Subtract(5, 3);
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void Divide_PositiveNumbers_ReturnsQuotient()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.Divide(6, 3);
+            // Assert
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void Divide_DivideByZero_ThrowsDivideByZeroException()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual + Assert 1
+           var ex = Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(3, 0));
+            // Assert 2
+            Assert.AreEqual("Cannot divide by zero.", ex.Message);
+        }
+
+        [TestMethod]
+        public void Multiply_TwoNumbers_ReturnsProduct()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.Multiply(2, 3);
+            // Assert
+            Assert.AreEqual(6, actual);
+        }
+
+        [TestMethod]
+        public void PowerOf_ExponentZero_ReturnsOne()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.PowerOf(21, 0);
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void PowerOf_PositiveExponent_ReturnsCorrectValue()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.PowerOf(2, 3);
+            // Assert
+            Assert.AreEqual(8, actual);
+        }
+    }
+}
