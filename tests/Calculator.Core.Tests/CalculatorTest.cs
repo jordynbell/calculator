f19cbc3aa9ae5e25752e465cfdb2f80d28f01580
Jordyn Bell <Jordyn@Envisionafrica.co.za>
2025-12-03 14:43:48 +0200

    Added minor functionality such as extra PowerOf test cases, and a SquareRoot function.

--- tests/Calculator.Core.Tests/CalculatorTest.cs
+++ tests/Calculator.Core.Tests/CalculatorTest.cs
@@ -42,7 +42,7 @@
             // Arrange
             var calculator = new Core.Calculator();
             // Actual + Assert 1
-           var ex = Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(3, 0));
+            var ex = Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(3, 0));
             // Assert 2
             Assert.AreEqual("Cannot divide by zero.", ex.Message);
         }
@@ -78,6 +78,59 @@
             var actual = calculator.PowerOf(2, 3);
             // Assert
             Assert.AreEqual(8, actual);
+        }
+
+        [TestMethod]
+        public void PowerOf_NegativeExponent_ReturnsCorrectValue()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.PowerOf(2, -2);
+            // Assert
+            Assert.AreEqual(0.25, actual);
+        }
+
+        [TestMethod]
+        public void PowerOf_ZeroBasePositiveExponent_ReturnsZero()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.PowerOf(0, 5);
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void PowerOf_ZeroBaseZeroExponent_ReturnsOne()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.PowerOf(0, 0);
+            // Assert
+            Assert.AreEqual(1, actual);
+        }
+        [TestMethod]
+        public void SquareRoot_PositiveNumber_ReturnsCorrectValue()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual
+            var actual = calculator.SquareRoot(16);
+            // Assert
+            Assert.AreEqual(4, actual);
+        }
+        [TestMethod]
+        public void SquareRoot_NegativeNumber_ThrowsArgumentException()
+        {
+            // Arrange
+            var calculator = new Core.Calculator();
+            // Actual + Assert 1
+            var ex = Assert.ThrowsException<ArgumentException>(() => calculator.SquareRoot(-4));
+            // Assert 2
+            Assert.AreEqual("Cannot calculate square root of a negative number.", ex.Message);
         }
     }
 }
